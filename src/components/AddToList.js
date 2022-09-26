b60074dda5ab2eaf8eafdbfc5c99e05e4cbd41e8
ellisvelandia <eyis619@gmail.com>
2022-09-25 20:28:27 -0500

    apianime

---
+++ src/components/AddToList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+
+export const AddToList = () => {
+  return (
+    <>
+      <div className="mylist">
+        <p>Add To List +</p>
+      </div>
+    </>
+  );
+};
